7395090d17736b62d3eb1ba57b7ddab8ed441db9
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-01-13 15:08:02 +0100

    code formatting + PAL copyright

--- src/hri_skeletons.cpp
+++ src/hri_skeletons.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2021, PAL Robotics
  * Copyright (c) 2008, Willow Garage, Inc.
  * All rights reserved.
  *
@@ -47,235 +48,241 @@ void linkUpdaterStatusFunction(StatusProperty::Level level,
                                const std::string& link_name,
                                const std::string& text,
                                HumansModelDisplay* display) {
-  display->setStatus(level, QString::fromStdString(link_name),
-                     QString::fromStdString(text));
+    display->setStatus(level, QString::fromStdString(link_name),
+                       QString::fromStdString(text));
 }
 
 HumansModelDisplay::HumansModelDisplay()
     : Display(), has_new_transforms_(false), time_since_last_transform_(0.0f) {
-  visual_enabled_property_ =
-      new Property("Visual Enabled", true,
-                   "Whether to display the visual representation of the robot.",
-                   this, SLOT(updateVisualVisible()));
+    visual_enabled_property_ = new Property(
+        "Visual Enabled", true,
+        "Whether to display the visual representation of the robot.", this,
+        SLOT(updateVisualVisible()));
 
-  collision_enabled_property_ = new Property(
-      "Collision Enabled", false,
-      "Whether to display the collision representation of the robot.", this,
-      SLOT(updateCollisionVisible()));
+    collision_enabled_property_ = new Property(
+        "Collision Enabled", false,
+        "Whether to display the collision representation of the robot.", this,
+        SLOT(updateCollisionVisible()));
 
-  update_rate_property_ =
-      new FloatProperty("Update Interval", 0,
-                        "Interval at which to update the links, in seconds. "
-                        "0 means to update every update cycle.",
-                        this);
+    update_rate_property_ =
+        new FloatProperty("Update Interval", 0,
+                          "Interval at which to update the links, in seconds. "
+                          "0 means to update every update cycle.",
+                          this);
 
-  update_rate_property_->setMin(0);
+    update_rate_property_->setMin(0);
 
-  alpha_property_ = new FloatProperty(
-      "Alpha", 1, "Amount of transparency to apply to the links.", this,
-      SLOT(updateAlpha()));
-  alpha_property_->setMin(0.0);
-  alpha_property_->setMax(1.0);
+    alpha_property_ = new FloatProperty(
+        "Alpha", 1, "Amount of transparency to apply to the links.", this,
+        SLOT(updateAlpha()));
+    alpha_property_->setMin(0.0);
+    alpha_property_->setMax(1.0);
 
-  tf_prefix_property_ =
-      new StringProperty("TF Prefix", "",
-                         "Robot Model normally assumes the link name is the "
-                         "same as the tf frame name. "
-                         " This option allows you to set a prefix.  Mainly "
-                         "useful for multi-robot situations.",
-                         this, SLOT(updateTfPrefix()));
+    tf_prefix_property_ =
+        new StringProperty("TF Prefix", "",
+                           "Robot Model normally assumes the link name is the "
+                           "same as the tf frame name. "
+                           " This option allows you to set a prefix.  Mainly "
+                           "useful for multi-robot situations.",
+                           this, SLOT(updateTfPrefix()));
 
-  idsSub_ = update_nh_.subscribe("/humans/bodies/tracked", 1,
-                                 &HumansModelDisplay::idsCallback, this);
+    idsSub_ = update_nh_.subscribe("/humans/bodies/tracked", 1,
+                                   &HumansModelDisplay::idsCallback, this);
 }
 
 HumansModelDisplay::~HumansModelDisplay() {
-  if (initialized()) {
-    for (std::map<std::string, Robot*>::iterator it = humans_.begin();
-         it != humans_.end(); it++)
-      delete it->second;
-  }
+    if (initialized()) {
+        for (std::map<std::string, Robot*>::iterator it = humans_.begin();
+             it != humans_.end(); it++)
+            delete it->second;
+    }
 }
 
 void HumansModelDisplay::onInitialize() {
-  updateVisualVisible();
-  updateCollisionVisible();
-  updateAlpha();
+    updateVisualVisible();
+    updateCollisionVisible();
+    updateAlpha();
 }
 
 void HumansModelDisplay::updateAlpha() {
-  for (std::map<std::string, Robot*>::iterator it = humans_.begin();
-       it != humans_.end(); it++)
-    it->second->setAlpha(alpha_property_->getFloat());
-  context_->queueRender();
+    for (std::map<std::string, Robot*>::iterator it = humans_.begin();
+         it != humans_.end(); it++)
+        it->second->setAlpha(alpha_property_->getFloat());
+    context_->queueRender();
 }
 
 void HumansModelDisplay::updateVisualVisible() {
-  for (std::map<std::string, Robot*>::iterator it = humans_.begin();
-       it != humans_.end(); it++)
-    it->second->setVisualVisible(visual_enabled_property_->getValue().toBool());
-  context_->queueRender();
+    for (std::map<std::string, Robot*>::iterator it = humans_.begin();
+         it != humans_.end(); it++)
+        it->second->setVisualVisible(
+            visual_enabled_property_->getValue().toBool());
+    context_->queueRender();
 }
 
 void HumansModelDisplay::updateCollisionVisible() {
-  for (std::map<std::string, Robot*>::iterator it = humans_.begin();
-       it != humans_.end(); it++)
-    it->second->setVisualVisible(
-        collision_enabled_property_->getValue().toBool());
-  context_->queueRender();
+    for (std::map<std::string, Robot*>::iterator it = humans_.begin();
+         it != humans_.end(); it++)
+        it->second->setVisualVisible(
+            collision_enabled_property_->getValue().toBool());
+    context_->queueRender();
 }
 
 void HumansModelDisplay::updateTfPrefix() {
-  clearStatuses();
-  context_->queueRender();
+    clearStatuses();
+    context_->queueRender();
 }
 
 void HumansModelDisplay::initializeRobot(
     std::map<std::string, Robot*>::iterator it) {
-  context_->queueRender();
+    context_->queueRender();
 
-  std::string description = "human_description_" + (it->first);
-  std::string content;
-  it->second = new Robot(scene_node_, context_, "Human: " + it->first, this);
+    std::string description = "human_description_" + (it->first);
+    std::string content;
+    it->second = new Robot(scene_node_, context_, "Human: " + it->first, this);
 
-  try {
-    if (!update_nh_.getParam(description,
-                             content))  // In content we get the string
-                                        // representing the urdf model
-    {
-      std::string loc;
-      if (update_nh_.searchParam(description, loc))
-        update_nh_.getParam(loc, content);
-      else {
+    try {
+        if (!update_nh_.getParam(description,
+                                 content))  // In content we get the string
+                                            // representing the urdf model
+        {
+            std::string loc;
+            if (update_nh_.searchParam(description, loc))
+                update_nh_.getParam(loc, content);
+            else {
+                clear();
+                setStatus(
+                    StatusProperty::Error, "URDF",
+                    QString(
+                        "Parameter [%1] does not exist, and was not found by "
+                        "searchParam()")
+                        .arg(QString::fromStdString(description)));
+                // try again in a second
+                // QTimer::singleShot(1000, this,
+                // SLOT(updateRobotDescription()));
+                return;
+            }
+        }
+    } catch (const ros::InvalidNameException& e) {
         clear();
         setStatus(StatusProperty::Error, "URDF",
-                  QString("Parameter [%1] does not exist, and was not found by "
-                          "searchParam()")
-                      .arg(QString::fromStdString(description)));
-        // try again in a second
-        // QTimer::singleShot(1000, this, SLOT(updateRobotDescription()));
+                  QString("Invalid parameter name: %1.\n%2")
+                      .arg(QString::fromStdString(description), e.what()));
         return;
-      }
     }
-  } catch (const ros::InvalidNameException& e) {
-    clear();
-    setStatus(StatusProperty::Error, "URDF",
-              QString("Invalid parameter name: %1.\n%2")
-                  .arg(QString::fromStdString(description), e.what()));
-    return;
-  }
 
-  if (content.empty()) {
-    clear();
-    setStatus(StatusProperty::Error, "URDF", "URDF is empty");
-    return;
-  }
+    if (content.empty()) {
+        clear();
+        setStatus(StatusProperty::Error, "URDF", "URDF is empty");
+        return;
+    }
 
-  std::string robot_description = content;
+    std::string robot_description = content;
 
-  urdf::Model descr;
-  if (!descr.initString(robot_description)) {
-    clear();
-    setStatus(StatusProperty::Error, "URDF", "Failed to parse URDF model");
-    return;
-  }
+    urdf::Model descr;
+    if (!descr.initString(robot_description)) {
+        clear();
+        setStatus(StatusProperty::Error, "URDF", "Failed to parse URDF model");
+        return;
+    }
 
-  setStatus(StatusProperty::Ok, "URDF", "URDFs parsed OK");
-  it->second->load(descr);
-  std::stringstream ss;
-  for (const auto& name_link_pair : it->second->getLinks()) {
-    const std::string& err = name_link_pair.second->getGeometryErrors();
-    if (!err.empty())
-      ss << "\n• for link '" << name_link_pair.first << "':\n" << err;
-  }
-  if (ss.tellp())
-    setStatus(StatusProperty::Error, "URDF",
-              QString("Errors loading geometries:").append(ss.str().c_str()));
+    setStatus(StatusProperty::Ok, "URDF", "URDFs parsed OK");
+    it->second->load(descr);
+    std::stringstream ss;
+    for (const auto& name_link_pair : it->second->getLinks()) {
+        const std::string& err = name_link_pair.second->getGeometryErrors();
+        if (!err.empty())
+            ss << "\n• for link '" << name_link_pair.first << "':\n" << err;
+    }
+    if (ss.tellp())
+        setStatus(
+            StatusProperty::Error, "URDF",
+            QString("Errors loading geometries:").append(ss.str().c_str()));
 
-  it->second->update(
-      TFLinkUpdater(context_->getFrameManager(),
-                    boost::bind(linkUpdaterStatusFunction, _1, _2, _3, this),
-                    tf_prefix_property_->getStdString()));
+    it->second->update(
+        TFLinkUpdater(context_->getFrameManager(),
+                      boost::bind(linkUpdaterStatusFunction, _1, _2, _3, this),
+                      tf_prefix_property_->getStdString()));
 }
 
 void HumansModelDisplay::onEnable() {
-  for (std::map<std::string, Robot*>::iterator it = humans_.begin();
-       it != humans_.end(); it++)
-    it->second->setVisible(true);
+    for (std::map<std::string, Robot*>::iterator it = humans_.begin();
+         it != humans_.end(); it++)
+        it->second->setVisible(true);
 }
 
 void HumansModelDisplay::onDisable() {
-  // robot_->setVisible(false);
-  for (std::map<std::string, Robot*>::iterator it = humans_.begin();
-       it != humans_.end(); it++)
-    it->second->setVisible(false);
-  // clear();
+    // robot_->setVisible(false);
+    for (std::map<std::string, Robot*>::iterator it = humans_.begin();
+         it != humans_.end(); it++)
+        it->second->setVisible(false);
+    // clear();
 }
 
 void HumansModelDisplay::update(float wall_dt, float /*ros_dt*/) {
-  time_since_last_transform_ += wall_dt;
-  float rate = update_rate_property_->getFloat();
-  bool update = rate < 0.0001f || time_since_last_transform_ >= rate;
+    time_since_last_transform_ += wall_dt;
+    float rate = update_rate_property_->getFloat();
+    bool update = rate < 0.0001f || time_since_last_transform_ >= rate;
 
-  if (has_new_transforms_ || update) {
-    for (std::map<std::string, Robot*>::iterator it = humans_.begin();
-         it != humans_.end(); it++)
-      it->second->update(TFLinkUpdater(
-          context_->getFrameManager(),
-          boost::bind(linkUpdaterStatusFunction, _1, _2, _3, this),
-          tf_prefix_property_->getStdString()));
-    context_->queueRender();
+    if (has_new_transforms_ || update) {
+        for (std::map<std::string, Robot*>::iterator it = humans_.begin();
+             it != humans_.end(); it++)
+            it->second->update(TFLinkUpdater(
+                context_->getFrameManager(),
+                boost::bind(linkUpdaterStatusFunction, _1, _2, _3, this),
+                tf_prefix_property_->getStdString()));
+        context_->queueRender();
 
-    has_new_transforms_ = false;
-    time_since_last_transform_ = 0.0f;
-  }
+        has_new_transforms_ = false;
+        time_since_last_transform_ = 0.0f;
+    }
 }
 
 void HumansModelDisplay::fixedFrameChanged() { has_new_transforms_ = true; }
 
 void HumansModelDisplay::clear() {
-  // robot_->clear();
-  for (std::map<std::string, Robot*>::iterator it = humans_.begin();
-       it != humans_.end(); it++)
-    it->second->clear();
-  clearStatuses();
-  robot_description_.clear();
+    // robot_->clear();
+    for (std::map<std::string, Robot*>::iterator it = humans_.begin();
+         it != humans_.end(); it++)
+        it->second->clear();
+    clearStatuses();
+    robot_description_.clear();
 }
 
 void HumansModelDisplay::reset() {
-  Display::reset();
-  has_new_transforms_ = true;
+    Display::reset();
+    has_new_transforms_ = true;
 }
 
 void HumansModelDisplay::idsCallback(const hri_msgs::IdsListConstPtr& msg) {
-  if (ros::ok()) {
-    ids_ = msg->ids;
+    if (ros::ok()) {
+        ids_ = msg->ids;
 
-    // Check for bodies that are no more in the list
-    // Remove them from the map
+        // Check for bodies that are no more in the list
+        // Remove them from the map
 
-    std::map<std::string, Robot*>::iterator itH;
-    for (itH = humans_.begin(); itH != humans_.end();) {
-      if (std::find(ids_.begin(), ids_.end(), itH->first) == ids_.end()) {
-        delete itH->second;
-        humans_.erase((itH++)->first);
-      } else
-        ++itH;
+        std::map<std::string, Robot*>::iterator itH;
+        for (itH = humans_.begin(); itH != humans_.end();) {
+            if (std::find(ids_.begin(), ids_.end(), itH->first) == ids_.end()) {
+                delete itH->second;
+                humans_.erase((itH++)->first);
+            } else
+                ++itH;
+        }
+
+        // Check for new faces
+        // Create a bounding box message and insert it in the map
+
+        std::pair<std::map<std::string, Robot*>::iterator, bool> ins;
+        std::vector<std::string>::iterator itS;
+        for (itS = ids_.begin(); itS != ids_.end(); ++itS) {
+            if (humans_.find(*itS) == humans_.end()) {
+                std::string id = *itS;
+                ins =
+                    humans_.insert(std::pair<std::string, Robot*>(id, nullptr));
+                if (ins.second) initializeRobot(ins.first);
+            }
+        }
     }
-
-    // Check for new faces
-    // Create a bounding box message and insert it in the map
-
-    std::pair<std::map<std::string, Robot*>::iterator, bool> ins;
-    std::vector<std::string>::iterator itS;
-    for (itS = ids_.begin(); itS != ids_.end(); ++itS) {
-      if (humans_.find(*itS) == humans_.end()) {
-        std::string id = *itS;
-        ins = humans_.insert(std::pair<std::string, Robot*>(id, nullptr));
-        if (ins.second) initializeRobot(ins.first);
-      }
-    }
-  }
 }
 
 }  // namespace rviz
